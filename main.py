a6d0e86e5e1787aa06c8cedfe7b53b2a8c0ffea0
Kyu Lee <kwooler@gmail.com>
2023-01-26 11:22:00 -0800

    CS7A01

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-##################################################
-# Comlete your code here
-##################################################
-
-# numbers = list(map(int, input().split()))
-
 def main():
-    main.numbers = list(map(int, input().split()))
-
-    ith = int(input())
-
-    # Code your program here
-
-    print(main.numbers)
-
-##
+    ##################################################
+    # Comlete your code here
+    ##################################################
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -7,8 +7,8 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '5 4 3 2 1\n1'
-    sys.stdin = io.StringIO(datastr)
+    # datastr = '5 4 3 2 1\n1'
+    # sys.stdin = io.StringIO(datastr)
 
     main.main()
     sys.stdout = sys.__stdout__
@@ -16,43 +16,22 @@ def test_main_1():
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    # regex_string = r'[\w,\W]*1'
-    # regex_string += r'[\w,\W]*3'
-    # regex_string += r'[\w,\W]*5'
-    # regex_string += r'[\w,\W]*'
-    # print(regex_string)
-    # res = re.search(regex_string, main.evenlist)
-    # assert res != None
+    regex_string = r'[\w,\W]*ello'
+    regex_string += r'[\w,\W]*orld'
+    regex_string += r'[\w,\W]*'
+    print(regex_string)
+    res = re.search(regex_string, lines[0])
+    assert res != None
+
+    regex_string = r'[\w,\W]*CS7'
+    regex_string += r'[\w,\W]*Introduction'
+    regex_string += r'[\w,\W]*'
+    print(regex_string)
+    res = re.search(regex_string, lines[1])
+    assert res != None
     # print(res.group())
-    assert main.main.numbers[0] == 1
-    assert main.main.numbers[1] == 4
-    assert main.main.numbers[2] == 3
-    assert main.main.numbers[3] == 2
-    assert main.main.numbers[4] == 5
-
-
-def test_main_2():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    datastr = '5 25 15 10 0\n3'
-    sys.stdin = io.StringIO(datastr)
-
-    main.main()
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
-
-    # regex_string = r'[\w,\W]*1'
-    # regex_string += r'[\w,\W]*3'
-    # regex_string += r'[\w,\W]*5'
-    # regex_string += r'[\w,\W]*'
-    # print(regex_string)
-    # res = re.search(regex_string, main.evenlist)
-    # assert res != None
-    # print(res.group())
-    assert main.main.numbers[0] == 0
-    assert main.main.numbers[1] == 5
-    assert main.main.numbers[2] == 10
-    assert main.main.numbers[3] == 15
-    assert main.main.numbers[4] == 25
+    # assert main.main.numbers[0] == 1
+    # assert main.main.numbers[1] == 4
+    # assert main.main.numbers[2] == 3
+    # assert main.main.numbers[3] == 2
+    # assert main.main.numbers[4] == 5
